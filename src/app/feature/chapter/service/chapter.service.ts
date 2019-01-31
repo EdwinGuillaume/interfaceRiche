4f9485f077e28a3f49eec4498dfc2fa542ec4e2d
Edwin <eguillau@enssat.Fr>
2019-01-31 10:17:17 +0100

    Début service

--- src/app/feature/chapter/service/chapter.service.ts
+++ src/app/feature/chapter/service/chapter.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {BehaviorSubject, Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChapterService {
 
+  // chapterList:
+  activeChapterSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
+
   constructor() { }
+
+  getActiveChatper(): Observable<string> {
+    return this.activeChapterSubject.asObservable();
+  }
 }
