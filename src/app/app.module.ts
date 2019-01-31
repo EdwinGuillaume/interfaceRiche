e842fa15e3ded58e2e1489974c6d77aa86ca4ade
Edwin <eguillau@enssat.Fr>
2019-01-31 09:58:22 +0100

    init chapter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { VideoComponent } from './video/video.component';
+import { ChapterComponent } from './chapter/chapter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VideoComponent
+    VideoComponent,
+    ChapterComponent
   ],
   imports: [
     BrowserModule
